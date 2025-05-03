0fd9657948aa81cc380918b9339a2cbcfecd35bc
testacc <testacc@testaccs-MacBook-Pro.local>
2025-05-03 18:51:13 -0400

    Update crash_util.py

--- pathbuilder/scripts/crash_util.py
+++ pathbuilder/scripts/crash_util.py
@@ -12,7 +12,7 @@ def add_crashes(INPUT_GEOJSON, segments):
 
     for segment in segments:
         projected_segment = transform(projection.transform, segment[0])
-        buffer = projected_segment.buffer(1200)
+        buffer = projected_segment.buffer(12000)
         segment[6] = len(crashes_index.query(buffer, predicate='intersects'))
         segment.pop(0)
 
